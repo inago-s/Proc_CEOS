50d24f92a9631455838ff06ad60b4ba0a829d1a6
i175s <i175s@icloud.com>
2021-01-07 12:10:08 +0900

    change main

--- sar2pauli.py
+++ sar2pauli.py
@@ -1,10 +1,10 @@
+import os
 import struct
 import sys
-import os
 
+import matplotlib.pyplot as plt
 import numpy as np
-from osgeo import gdal
-from osgeo import osr
+from osgeo import gdal, osr
 
 
 def get_fileinfo(folder_name):
@@ -54,29 +54,24 @@ def get_fileinfo(folder_name):
     return HH_file, HV_file, VV_file, VH_file, nline, ncell, LED_file
 
 
-def get_gcp(LED, L, P):
-    with open(LED, mode='rb') as f:
-        f.seek(1604432+1024)
-        # 緯度変換係数
-        coefficient_lat = [float(f.read(20)) for i in range(25)]
-        # 経度変換係数
-        coefficient_lon = [float(f.read(20)) for i in range(25)]
-
-        gcp_L = np.linspace(0, L, int((L/1000)+1), dtype='int')
-        gcp_P = np.linspace(0, P, int((P/1000)+1), dtype='int')
-        gcp = []
-
+def get_gcp(file, L, P):
+    gcp = []
+    with open(file, mode='rb') as f:
+        gcp_L = np.linspace(0, L-1, int((L/1000)+1), dtype='int')
         for line in gcp_L:
-            for pixel in gcp_P:
-                l_matrix = np.array([line**4, line**3, line**2, line**1, 1])
-                p_matrix = np.array(
-                    [[pixel**4], [pixel**3], [pixel**2], [pixel**1], [1]])
-                lp_matrix = np.ravel(l_matrix*p_matrix)
-                gcp.append(gdal.GCP(np.dot(coefficient_lon, lp_matrix),
-                                    np.dot(coefficient_lat, lp_matrix),
-                                    0.0, float(pixel), float(line)))
+            f.seek(720+(line)*(P*8+544)+192)
+            f_lat = float(struct.unpack(">%s" % "i", f.read(4))[0])/1000000
+            m_lat = float(struct.unpack(">%s" % "i", f.read(4))[0])/1000000
+            e_lat = float(struct.unpack(">%s" % "i", f.read(4))[0])/1000000
+            f_lon = float(struct.unpack(">%s" % "i", f.read(4))[0])/1000000
+            m_lon = float(struct.unpack(">%s" % "i", f.read(4))[0])/1000000
+            e_lon = float(struct.unpack(">%s" % "i", f.read(4))[0])/1000000
 
-        return gcp
+            gcp.append(gdal.GCP(f_lon, f_lat, 0.0, float(0), float(line)))
+            gcp.append(gdal.GCP(m_lon, m_lat, 0.0, float((P/2)), float(line)))
+            gcp.append(gdal.GCP(e_lon, e_lat, 0.0, float(P), float(line)))
+
+    return gcp
 
 
 def read_bin(file, h, nrec):
@@ -97,36 +92,27 @@ def Process_Pauli(pic, nlines, npixels, HH, HV, VV, VH, CF):
             read_bin(HV, h, nrec), read_bin(VV, h, nrec),\
             read_bin(VH, h, nrec)
 
-        # r, g, b = abs(slc_hh-slc_hv), abs(slc_hv+slc_vh), abs(slc_hh+slc_vv)
+        r, g, b = abs(slc_hh-slc_hv), abs(slc_hv+slc_vh), abs(slc_hh+slc_vv)
+
+        '''
         slc_hh = 20*log10(abs(slc_hh)) + CF - 32.0
         slc_hv = 20*log10(abs(slc_hh)) + CF - 32.0
         slc_vv = 20*log10(abs(slc_hh)) + CF - 32.0
         slc_vh = 20*log10(abs(slc_hh)) + CF - 32.0
 
         r, g, b = slc_hh-slc_hv, slc_hv+slc_vh, slc_hh+slc_vv
+        '''
+
         pic[h] = np.stack([r, g, b], 2)
-
-
-def Process_SLC(pic, nlines, npixels, HH, HV, VV, VH, CF):
-    nrec = 544+npixels*8
-    for h in range(nlines):
-        slc_hh, slc_hv, slc_vv, slc_vh = read_bin(HH, h, nrec),\
-            read_bin(HV, h, nrec), read_bin(VV, h, nrec),\
-            read_bin(VH, h, nrec)
-
-        slc_hh = 20*log10(abs(slc_hh)) + CF - 32.0
-        slc_hv = 20*log10(abs(slc_hh)) + CF - 32.0
-        slc_vv = 20*log10(abs(slc_hh)) + CF - 32.0
-        slc_vh = 20*log10(abs(slc_hh)) + CF - 32.0
 
 
 def make_Tiff(img, gcpList, band, folder_name):
     dtype = gdal.GDT_Byte
     output = gdal.GetDriverByName('GTiff').Create(
-        folder_name+'test.tif', img.shape[1], img.shape[0],
+        folder_name+'test2.tif', img.shape[1], img.shape[0],
         band, dtype, options=['PHOTOMETRIC=RGB'])
     gcp_srs = osr.SpatialReference()
-    gcp_srs.ImportFromEPSG(3717)
+    gcp_srs.ImportFromEPSG(4612)
     gcp_crs_wkt = gcp_srs.ExportToWkt()
     output.SetGCPs(gcpList, gcp_crs_wkt)
 
@@ -145,7 +131,7 @@ def main():
     else:
         folder_name = sys.argv[1]
 
-    HH_file, HV_file, VV_file, VH_file, nlines, npixels, LED_file = \
+    HH_file, HV_file, VV_file, VH_file, nlines, npixels, LED_file =\
         get_fileinfo(folder_name)
 
     with open(LED_file, mode='rb') as f:
@@ -154,7 +140,8 @@ def main():
 
     pic = np.empty((nlines, npixels, 3), dtype=np.float32)
 
-    Process_Pauli(pic, nlines, npixels, HH_file, HV_file, VV_file, VH_file, CF)
+    Process_Pauli(pic, nlines, npixels, HH_file,
+                  HV_file, VV_file, VH_file, CF)
 
     max_r = np.percentile(pic[:, :, 0], q=95)
     max_g = np.percentile(pic[:, :, 1], q=95)
@@ -164,7 +151,7 @@ def main():
     pic[:, :, 1] = np.clip((pic[:, :, 1]/max_g)*255, 0, 255).astype('uint8')
     pic[:, :, 2] = np.clip((pic[:, :, 2]/max_b)*255, 0, 255).astype('uint8')
 
-    gcpList = get_gcp(LED_file, nlines, npixels)
+    gcpList = get_gcp(HH_file, nlines, npixels)
 
     make_Tiff(pic, gcpList, 3, folder_name)
 
